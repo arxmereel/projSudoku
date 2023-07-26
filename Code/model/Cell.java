1e7543b576b3607a92e26f6be3683d6a81fa0a05
Max Tung <1228trc@gmail.com>
2023-07-25 19:30:25 -0700

    Update Cell Temp Completion
    Finished adding setters and getters.
    Candidates now changed to HashSet from Basic Array for optimization reasons.
    
    Added another constructor so it MAY be easier to create a board with Static Cells and dynamic (solvable) Cells

--- Code/model/Cell.java
+++ Code/model/Cell.java
@@ -1,21 +1,68 @@
-/**
+import java.util.HashSet;
+import java.util.Set;
+
+/** Basic code for the Cell Class.
+ * 
  * @author maxtung
  */
 
 public class Cell {
     protected boolean editable;
     protected int value;
-    protected int[] candidates;
+    protected Set<Integer> candidates;
 
-    public Cell(int val) {
+    //DEFAULTS Cell value to ZERO.
+    public Cell() {
         this.editable = true;
+        this.value = 0;
+        this.candidates = new HashSet<>();
+    }
+
+    //DEFAULTS Cell value to ZERO.
+    public Cell(int val) {
+        this.editable = false;
         this.value = val;
-        this.candidates = new int[0];
+        this.candidates = new HashSet<>();
     }
 
     public boolean isEditable() {
         return editable;
     }
 
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    //Setter. Will return value if cell is editable, -1 if not.
+    public int setValue(int val){
+        if (editable){
+            this.value = val;
+            return val;
+        } else{
+            return -1;
+        }
+    }
+
+    public Set<Integer> getCandidates() {
+        return candidates;
+    }
+
+    public void setCandidates(int[] newCandidates) {
+        for (int candidate : newCandidates) {
+            this.candidates.add(candidate);
+        }
+    }
+
+    public void clearCandidates(){
+        this.candidates = new HashSet<>();
+    } 
+
+    public void delCandidate(int del){
+        this.candidates.remove(del);
+    }
 
 }

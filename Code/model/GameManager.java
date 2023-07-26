4752e026120be6c1342f521970dfb45de3c4715b
joe12334667 <joe12334667@gmail.com>
2023-07-26 14:21:05 -0700

    finish remove number

--- Code/model/GameManager.java
+++ Code/model/GameManager.java
@@ -1,4 +1,5 @@
 package model;
+
 import java.util.Random;
 
 /**
@@ -9,54 +10,57 @@ import java.util.Random;
 
 public class GameManager {
     protected Board gameBoard;
+    protected Board answerBoard;
     private static final int SIZE = 9;
     private static final int BOX_SIZE = 3;
     private static final int EMPTY_CELL = 0;
+    private static final int CELL_TO_REMOVE = 40; // 50 = hard, 40 = normal
     private Random random;
 
-    public GameManager(){
+    public GameManager() {
         this.gameBoard = new Board();
         this.random = new Random();
     }
 
+    // Create a dupe board, try the move, if valid, make edit to actual board and
+    // return true. If not, return false.
 
-    //Create a dupe board, try the move, if valid, make edit to actual board and return true. If not, return false.
+    // IMPORTANT PSA: A move can be valid but still be the incorrect move
+    // Possbily generate whole puzzle and keep a backup board to directly check
+    // answers in place???
 
-    //IMPORTANT PSA: A move can be valid but still be the incorrect move
-    //Possbily generate whole puzzle and keep a backup board to directly check answers in place???
-
-    public boolean makeMove(int row, int col, int val){
+    public boolean makeMove(int row, int col, int val) {
         Board tempBoard = new Board(this.gameBoard);
 
-            Section rowSection = new Section(tempBoard.getRow(row));
-            Section colSection = new Section(tempBoard.getCol(col));
-            Section secSection = new Section(tempBoard.getSection(row, col));
+        Section rowSection = new Section(tempBoard.getRow(row));
+        Section colSection = new Section(tempBoard.getCol(col));
+        Section secSection = new Section(tempBoard.getSection(row, col));
 
-            if(rowSection.isValid() && colSection.isValid() && secSection.isValid()){
-                gameBoard.getCell(row, col).setValue(val);
-            }
+        if (rowSection.isValid() && colSection.isValid() && secSection.isValid()) {
+            gameBoard.getCell(row, col).setValue(val);
+        }
         return false;
     }
 
-    public void addCandidates(int row, int col, int[] cands){
+    public void addCandidates(int row, int col, int[] cands) {
         gameBoard.getCell(row, col).addCandidates(cands);
     }
 
-    public void clearCandidates(int row, int col){
+    public void clearCandidates(int row, int col) {
         gameBoard.getCell(row, col).clearCandidates();
     }
 
-    public void delCandidate(int row, int col, int del){
+    public void delCandidate(int row, int col, int del) {
         gameBoard.getCell(row, col).delCandidate(del);
     }
 
-    //for GUI Parese
-    public void getCellVal(int row, int col){
+    // for GUI Parese
+    public void getCellVal(int row, int col) {
         gameBoard.getCell(row, col).getValue();
     }
 
-    //for GUI Parese
-    public void getCellCands(int row, int col){
+    // for GUI Parese
+    public void getCellCands(int row, int col) {
         gameBoard.getCell(row, col).getCandidates();
     }
 
@@ -66,13 +70,15 @@ public class GameManager {
     public void generateSudoku() {
         // fillDiagonalBoxes();
         fillFirstRoll();
-        solveSudoku(0, 0);
+        solveSudoku(0, 0, gameBoard);
+        answerBoard = new Board(gameBoard);
         System.out.println(gameBoard);
-        // removeNumbers();
+        removeNumbers(gameBoard);
+        System.out.println(gameBoard);
     }
 
     /**
-     * Fills a 3x3 box in the Sudoku grid with random valid numbers.
+     * Fills the first roll in the Sudoku grid with random valid numbers from 1-9.
      *
      * @param row The starting row index of the box.
      * @param col The starting column index of the box.
@@ -80,9 +86,9 @@ public class GameManager {
     private void fillFirstRoll() {
         int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         shuffleArray(numbers);
-    
+
         for (int i = 0; i < 9; i++) {
-            gameBoard.getCell(0,i).setValue(numbers[i]);
+            gameBoard.getCell(0, i).setValue(numbers[i]);
             // grid[i][0] = numbers[index++];
         }
     }
@@ -108,23 +114,23 @@ public class GameManager {
      * @param col The column index of the cell to start solving from.
      * @return {@code true} if a solution is found, {@code false} otherwise.
      */
-    private boolean solveSudoku(int row, int col) {
+    private boolean solveSudoku(int row, int col, Board board) {
         if (row == SIZE) {
             row = 0;
             if (++col == SIZE) {
                 return true; // Reached the end of the grid
             }
         }
-        if (gameBoard.getCell(row, col).getValue() != EMPTY_CELL) {
-            return solveSudoku(row + 1, col);
+        if (board.getCell(row, col).getValue() != EMPTY_CELL) {
+            return solveSudoku(row + 1, col, board);
         }
         for (int num = 1; num <= SIZE; num++) {
-            if (isValidPlacement(row, col, num)) {
-                gameBoard.getCell(row, col).setValue(num);
-                if (solveSudoku(row + 1, col)) {
+            if (isValidPlacement(row, col, num, board)) {
+                board.getCell(row, col).setValue(num);
+                if (solveSudoku(row + 1, col, board)) {
                     return true;
                 }
-                gameBoard.getCell(row, col).setValue(EMPTY_CELL);
+                board.getCell(row, col).setValue(EMPTY_CELL);
             }
         }
         return false;
@@ -138,9 +144,9 @@ public class GameManager {
      * @param num The number to be placed.
      * @return {@code true} if the placement is valid, {@code false} otherwise.
      */
-    private boolean isValidPlacement(int row, int col, int num) {
+    private boolean isValidPlacement(int row, int col, int num, Board board) {
         for (int i = 0; i < SIZE; i++) {
-            if (gameBoard.getCell(row, i).getValue() == num || gameBoard.getCell(i, col).getValue() == num) {
+            if (board.getCell(row, i).getValue() == num || board.getCell(i, col).getValue() == num) {
                 return false;
             }
         }
@@ -148,7 +154,7 @@ public class GameManager {
         int boxColStart = col - col % BOX_SIZE;
         for (int i = boxRowStart; i < boxRowStart + BOX_SIZE; i++) {
             for (int j = boxColStart; j < boxColStart + BOX_SIZE; j++) {
-                if (gameBoard.getCell(i, j).getValue() == num) {
+                if (board.getCell(i, j).getValue() == num) {
                     return false;
                 }
             }
@@ -156,7 +162,67 @@ public class GameManager {
         return true;
     }
 
+    /**
+     * Removes numbers from the completed grid to achieve the desired difficulty
+     * level while ensuring a unique solution.
+     * 
+     * @param board The Sudoku board to remove numbers from.
+     */
+    private void removeNumbers(Board board) {
+        int count = 0;
 
-    
+        while (count < CELL_TO_REMOVE) {
+            int row = random.nextInt(SIZE);
+            int col = random.nextInt(SIZE);
+            Cell cell = board.getCell(row, col);
+
+            if (cell.getValue() != EMPTY_CELL) {
+                int temp = cell.getValue();
+                cell.setValue(EMPTY_CELL); // Temporarily remove the number from the cell
+
+                Board tempBoard = new Board(board);
+                int solutionCount = countSolutions(0, 0, tempBoard);
+
+                if (solutionCount == 1) {
+                    // Keep the removal since there is still a unique solution
+                    count++;
+                } else {
+                    // Undo the removal by restoring the original number in the cell
+                    cell.setValue(temp);
+                }
+            }
+        }
+    }
+
+    /**
+     * Counts the number of solutions for the current Sudoku board using
+     * backtracking.
+     *
+     * @param row   The row index of the cell to start solving from.
+     * @param col   The column index of the cell to start solving from.
+     * @param board The Sudoku board to count solutions for.
+     * @return The number of valid solutions for the Sudoku board.
+     */
+    private int countSolutions(int row, int col, Board board) {
+        if (row == SIZE) {
+            row = 0;
+            if (++col == SIZE) {
+                return 1; // Reached the end of the grid, a solution is found
+            }
+        }
+        if (board.getCell(row, col).getValue() != EMPTY_CELL) {
+            return countSolutions(row + 1, col, board);
+        }
+
+        int solutions = 0;
+        for (int num = 1; num <= SIZE; num++) {
+            if (isValidPlacement(row, col, num, board)) {
+                board.getCell(row, col).setValue(num);
+                solutions += countSolutions(row + 1, col, board);
+                board.getCell(row, col).setValue(EMPTY_CELL);
+            }
+        }
+        return solutions;
+    }
 
 }

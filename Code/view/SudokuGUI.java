dde60eb7fd5f69b25ede51a749506ffeea84bf56
dylanzwilson <140107967+dylanzwilson@users.noreply.github.com>
2023-07-27 18:39:18 -0700

    Update SudokuGUI.java
    Added functionality to buttons
    Added navigation of board with WASD or ctrl+arrow keys
    Added dynamic color change of current cell

--- Code/view/SudokuGUI.java
+++ Code/view/SudokuGUI.java
@@ -1,9 +1,12 @@
 /**
  * File: SudokuGUI.java
- * Author: Dylan Wilson
+ * @author: Dylan Wilson
  * Course: CSC335
  * Description:
  * Class for a GUI that plays Sudoku.
+ * WASD are used to navigate the board. Arrow keys can also be used, but CTRL must be held down.
+ * The current row, column, and section is highlighted in a light blue,
+ * and the current cell is highlighted in a slightly darker blue.
  */
 
 package view;
@@ -13,6 +16,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -34,6 +38,11 @@ public class SudokuGUI extends Application {
 	private Button newGame; // begins a new game
 	private Button modeToggle; // toggles between note-taking mode and answer mode
 	
+	private int col; // x value of current cell
+	private int row; // y value of current cell
+	
+	private boolean noteMode; // boolean to determine if note taking mode is on or off
+	
 	public static void main(String args[]) {
 		launch();
 	}
@@ -42,14 +51,19 @@ public class SudokuGUI extends Application {
 	public void start(Stage stage) {
 		stage.setTitle("Sudoku");
 		layoutGUI();
-		startGame();
 		
 		Scene scene = new Scene(mainPane, 1000, 600);
+		
+		registerKeyPresses(scene);
+		registerButtons();
+		newGame();
+		
 		stage.setScene(scene);
 		stage.show();
 	}
 	
 	private void layoutGUI() {
+		
 		mainPane = new GridPane();
 		boardPane = new GridPane();
 		buttonPane = new GridPane();
@@ -86,9 +100,9 @@ public class SudokuGUI extends Application {
 		// initialize cellLabels and add them to the board and set style
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				cellLabel[i][j] = new Label(String.valueOf(i*9 + j+1));
+				cellLabel[i][j] = new Label();
 				cellLabel[i][j].setStyle("-fx-font: normal bold 30px 'serif'; -fx-padding: 5px;"
-						+ "-fx-border-color: lightgray;");
+						+ "-fx-border-color: lightgray; -fx-background-color: white;");
 				sectionPane[j/3][i/3].add(cellLabel[i][j], j%3, i%3);
 				cellLabel[i][j].setMaxHeight(60);
 				cellLabel[i][j].setMinHeight(60);
@@ -102,7 +116,7 @@ public class SudokuGUI extends Application {
 				for (int m = 0; m < 3; m++) {
 					for (int n = 0; n < 3; n++) {
 						notesLabel[i][j][m][n] = new Label();
-						notesLabel[i][j][m][n].setStyle("-fx-font: normal 10px 'serif' lightgray;"
+						notesLabel[i][j][m][n].setStyle("-fx-font: normal 10px 'serif';"
 								+ "-fx-padding: 5px;");
 						cellPane[i][j].add(notesLabel[i][j][m][n], n, m);
 						notesLabel[i][j][m][n].setMaxHeight(20);
@@ -115,7 +129,7 @@ public class SudokuGUI extends Application {
 			}
 		}
 		
-		// arrange right side buttons and make them look pretty
+		// arrange number buttons and make them look pretty
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				numberButtons[i][j] = new Button(String.valueOf(i*3 + j+1));
@@ -153,20 +167,347 @@ public class SudokuGUI extends Application {
 		modeToggle.setMinWidth(152);
 	}
 	
-	private void startGame() {
+	/**
+	 * Registers buttons and assigns functions and events to each.
+	 */
+	private void registerButtons() {
+		newGame.setOnAction(e -> {
+			newGame();
+		});
+		modeToggle.setOnAction(e -> {
+			toggleMode();
+		});
+		numberButtons[0][0].setOnAction(e -> {
+			cellLabel[row][col].setText("1");
+		});
+		numberButtons[0][1].setOnAction(e -> {
+			cellLabel[row][col].setText("2");
+		});
+		numberButtons[0][2].setOnAction(e -> {
+			cellLabel[row][col].setText("3");
+		});
+		numberButtons[1][0].setOnAction(e -> {
+			cellLabel[row][col].setText("4");
+		});
+		numberButtons[1][1].setOnAction(e -> {
+			cellLabel[row][col].setText("5");
+		});
+		numberButtons[1][2].setOnAction(e -> {
+			cellLabel[row][col].setText("6");
+		});
+		numberButtons[2][0].setOnAction(e -> {
+			cellLabel[row][col].setText("7");
+		});
+		numberButtons[2][1].setOnAction(e -> {
+			cellLabel[row][col].setText("8");
+		});
+		numberButtons[2][2].setOnAction(e -> {
+			cellLabel[row][col].setText("9");
+		});
+	}
+	
+	/**
+	 * Registers the key presses and assigns key presses to functions.
+	 * @param scene: The scene object. Required to read key presses.
+	 */
+	private void registerKeyPresses(Scene scene) {
+		scene.setOnKeyPressed(e -> {
+			if (e.getCode() == KeyCode.LEFT || e.getCode() == KeyCode.A) {
+				moveCurr("left");
+			} else if (e.getCode() == KeyCode.RIGHT || e.getCode() == KeyCode.D) {
+				moveCurr("right");
+			} else if (e.getCode() == KeyCode.UP || e.getCode() == KeyCode.W) {
+				moveCurr("up");
+			} else if (e.getCode() == KeyCode.DOWN || e.getCode() == KeyCode.S) {
+				moveCurr("down");
+			} else if (e.getCode() == KeyCode.ALT) {
+				toggleMode();
+			} else if (e.getCode() == KeyCode.DIGIT1) {
+				cellLabel[row][col].setText("1");
+			} else if (e.getCode() == KeyCode.DIGIT2) {
+				cellLabel[row][col].setText("2");
+			} else if (e.getCode() == KeyCode.DIGIT3) {
+				cellLabel[row][col].setText("3");
+			} else if (e.getCode() == KeyCode.DIGIT4) {
+				cellLabel[row][col].setText("4");
+			} else if (e.getCode() == KeyCode.DIGIT5) {
+				cellLabel[row][col].setText("5");
+			} else if (e.getCode() == KeyCode.DIGIT6) {
+				cellLabel[row][col].setText("6");
+			} else if (e.getCode() == KeyCode.DIGIT7) {
+				cellLabel[row][col].setText("7");
+			} else if (e.getCode() == KeyCode.DIGIT8) {
+				cellLabel[row][col].setText("8");
+			} else if (e.getCode() == KeyCode.DIGIT9) {
+				cellLabel[row][col].setText("9");
+			}
+		});
+	}
+	
+	/**
+	 * Begins a new game. Unfinished.
+	 */
+	private void newGame() {
+		
+		// reset colors and labels
 		for (int i = 0; i < 9; i++) {
-			cellLabel[i][4].setStyle("-fx-background-color: #e3ecf3; -fx-font: normal bold 30px 'serif'; "
-					+ "-fx-padding: 5px;");
-			cellLabel[4][i].setStyle("-fx-background-color: #e3ecf3; -fx-font: normal bold 30px 'serif'; "
-					+ "-fx-padding: 5px;");
-		}
-		for (int i = 3; i < 6; i++) {
-			for (int j = 3; j < 6; j++) {
-				cellLabel[i][j].setStyle("-fx-background-color: #e3ecf3; -fx-font: normal bold 30px 'serif'; "
-						+ "-fx-padding: 5px;");
+			for (int j = 0; j < 9; j++) {
+				cellLabel[i][j].setText("");
+				cellLabel[i][j].setStyle("-fx-background-color: white; -fx-font: normal bold 30px 'serif';"
+						+ "-fx-padding: 5px; -fx-border-color: lightgray;");
 			}
 		}
-		cellLabel[4][4].setStyle("-fx-background-color: #c0dffb; -fx-font: normal bold 30px 'serif'; "
-					+ "-fx-padding: 5px;");
+		
+		// initialize variables and toggle button, or reset them
+		row = 4;
+		col = 4;
+		modeToggle.setStyle("-fx-background-color: white; -fx-text-fill: black;"
+				+ "-fx-font-size: 20");
+		modeToggle.setText("Notes: OFF");
+		noteMode = false;
+
+		addColor();
+		
+	}
+	
+	/**
+	 * Toggles between note-taking mode and answer mode.
+	 */
+	private void toggleMode() {
+		if (noteMode == false) {
+			modeToggle.setStyle("-fx-background-color: blue; -fx-text-fill: white;"
+					+ "-fx-font-size: 20");
+			modeToggle.setText("Notes: ON");
+			noteMode = true;
+		} else {
+			modeToggle.setStyle("-fx-background-color: white; -fx-text-fill: black;"
+					+ "-fx-font-size: 20");
+			modeToggle.setText("Notes: OFF");
+			noteMode = false;
+		}
+	}
+	
+	/**
+	 * Moves the current selected cell in the given direction.
+	 * Checks if the direction is valid, then changes the color of the cells.
+	 * @param direction, direction to move: left, right, up, down
+	 */
+	private void moveCurr(String direction) {
+		
+		// if direction is valid
+		if ((col != 0 && direction.equals("left")) || (col != 8 && direction.equals("right")) ||
+			(row != 0 && direction.equals("up")) || (row != 8 && direction.equals("down"))) {
+			
+			removeColor();
+			if (direction.equals("left")) col -= 1;
+			else if (direction.equals("right")) col += 1;
+			else if (direction.equals("up")) row -= 1;
+			else if (direction.equals("down")) row += 1;
+			addColor();
+			System.out.println("X: " + col + " Y: " + row);
+		}
+	}
+	
+	/**
+	 * Removes the color from the current cell's row, column, and section.
+	 */
+	private void removeColor() {
+		
+		// row and column
+				for (int i = 0; i < 9; i++) {
+					cellLabel[i][col].setStyle("-fx-background-color: white; -fx-font: normal bold 30px 'serif';"
+							+ "-fx-padding: 5px; -fx-border-color: lightgray;");
+					cellLabel[row][i].setStyle("-fx-background-color: white; -fx-font: normal bold 30px 'serif';"
+							+ "-fx-padding: 5px; -fx-border-color: lightgray;");
+					
+					// notesLabels as well
+					for (int m = 0; m < 3; m++) {
+						for (int n = 0; n < 3; n++) {
+							notesLabel[i][col][m][n].setStyle("-fx-background-color: white;"
+									+ "-fx-font: normal 10px 'serif'; -fx-padding: 5px;");
+							notesLabel[row][i][m][n].setStyle("-fx-background-color: white;"
+									+ "-fx-font: normal 10px 'serif'; -fx-padding: 5px;");
+						}
+					}
+				}
+				
+				// determine which section to color
+				int startX = 0, endX = 0, startY = 0, endY = 0;
+				if (col < 3 && row < 3) {
+					startX = 0;
+					endX = 3;
+					startY = 0;
+					endY = 3;
+				} else if ((col >= 3 && col < 6) && (row < 3)) {
+					startX = 3;
+					endX = 6;
+					startY = 0;
+					endY = 3;
+				} else if ((col >= 6) && (row < 3)) {
+					startX = 6;
+					endX = 9;
+					startY = 0;
+					endY = 3;
+				} else if ((col < 3) && (row >= 3 && row < 6)) {
+					startX = 0;
+					endX = 3;
+					startY = 3;
+					endY = 6;
+				} else if ((col >= 3 && col < 6) && (row >= 3 && row < 6)) {
+					startX = 3;
+					endX = 6;
+					startY = 3;
+					endY = 6;
+				} else if ((col >= 6) && (row >= 3 && row < 6)) {
+					startX = 6;
+					endX = 9;
+					startY = 3;
+					endY = 6;
+				} else if ((col < 3) && (row >= 6)) {
+					startX = 0;
+					endX = 3;
+					startY = 6;
+					endY = 9;
+				} else if ((col >= 3 && col < 6) && (row >= 6)) {
+					startX = 3;
+					endX = 6;
+					startY = 6;
+					endY = 9;
+				} else if ((col >= 6) && (row >= 6)) {
+					startX = 6;
+					endX = 9;
+					startY = 6;
+					endY = 9;
+				}
+				
+				// color that section
+				for (int i = startX; i < endX; i++) {
+					for (int j = startY; j < endY; j++) {
+						cellLabel[j][i].setStyle("-fx-background-color: white; -fx-font: normal bold 30px 'serif';"
+								+ "-fx-padding: 5px; -fx-border-color: lightgray;");
+						cellLabel[j][i].setStyle("-fx-background-color: white; -fx-font: normal bold 30px 'serif';"
+								+ "-fx-padding: 5px; -fx-border-color: lightgray;");
+						
+						// notesLabels as well
+						for (int m = 0; m < 3; m++) {
+							for (int n = 0; n < 3; n++) {
+								notesLabel[i][j][m][n].setStyle("-fx-background-color: white; -fx-border-color: lightgray;"
+										+ "-fx-font: normal 10px 'serif'; -fx-padding: 5px;");
+							}
+						}
+					}
+				}
+				
+				// current cell
+				cellLabel[row][col].setStyle("-fx-background-color: white; -fx-font: normal bold 30px 'serif'; "
+							+ "-fx-padding: 5px;");
+				for (int m = 0; m < 3; m++) {
+					for (int n = 0; n < 3; n++) {
+						notesLabel[row][col][m][n].setStyle("-fx-background-color: white;"
+								+ "-fx-font: normal 10px 'serif'; -fx-padding: 5px;");
+					}
+				}
+	}
+	
+	/**
+	 * Adds color to the current cell's row, column, and section.
+	 * Code is nearly identical to removeColor(), it just adds color instead.
+	 */
+	private void addColor() {
+		
+		// row and column
+		for (int i = 0; i < 9; i++) {
+			cellLabel[i][col].setStyle("-fx-background-color: #e3ecf3; -fx-font: normal bold 30px 'serif';"
+					+ "-fx-padding: 5px; -fx-border-color: lightgray;");
+			cellLabel[row][i].setStyle("-fx-background-color: #e3ecf3; -fx-font: normal bold 30px 'serif';"
+					+ "-fx-padding: 5px; -fx-border-color: lightgray;");
+			
+			// notesLabels as well
+			for (int m = 0; m < 3; m++) {
+				for (int n = 0; n < 3; n++) {
+					notesLabel[i][col][m][n].setStyle("-fx-background-color: #e3ecf3;"
+							+ "-fx-font: normal 10px 'serif'; -fx-padding: 5px;");
+					notesLabel[row][i][m][n].setStyle("-fx-background-color: #e3ecf3;"
+							+ "-fx-font: normal 10px 'serif'; -fx-padding: 5px;");
+				}
+			}
+		}
+		
+		// determine which section to color
+		int startX = 0, endX = 0, startY = 0, endY = 0;
+		if (col < 3 && row < 3) {
+			startX = 0;
+			endX = 3;
+			startY = 0;
+			endY = 3;
+		} else if ((col >= 3 && col < 6) && (row < 3)) {
+			startX = 3;
+			endX = 6;
+			startY = 0;
+			endY = 3;
+		} else if ((col >= 6) && (row < 3)) {
+			startX = 6;
+			endX = 9;
+			startY = 0;
+			endY = 3;
+		} else if ((col < 3) && (row >= 3 && row < 6)) {
+			startX = 0;
+			endX = 3;
+			startY = 3;
+			endY = 6;
+		} else if ((col >= 3 && col < 6) && (row >= 3 && row < 6)) {
+			startX = 3;
+			endX = 6;
+			startY = 3;
+			endY = 6;
+		} else if ((col >= 6) && (row >= 3 && row < 6)) {
+			startX = 6;
+			endX = 9;
+			startY = 3;
+			endY = 6;
+		} else if ((col < 3) && (row >= 6)) {
+			startX = 0;
+			endX = 3;
+			startY = 6;
+			endY = 9;
+		} else if ((col >= 3 && col < 6) && (row >= 6)) {
+			startX = 3;
+			endX = 6;
+			startY = 6;
+			endY = 9;
+		} else if ((col >= 6) && (row >= 6)) {
+			startX = 6;
+			endX = 9;
+			startY = 6;
+			endY = 9;
+		}
+		
+		// color that section
+		for (int i = startX; i < endX; i++) {
+			for (int j = startY; j < endY; j++) {
+				cellLabel[j][i].setStyle("-fx-background-color: #e3ecf3; -fx-font: normal bold 30px 'serif';"
+						+ "-fx-padding: 5px; -fx-border-color: lightgray;");
+				cellLabel[j][i].setStyle("-fx-background-color: #e3ecf3; -fx-font: normal bold 30px 'serif';"
+						+ "-fx-padding: 5px; -fx-border-color: lightgray;");
+				
+				// notesLabels as well
+				for (int m = 0; m < 3; m++) {
+					for (int n = 0; n < 3; n++) {
+						notesLabel[i][j][m][n].setStyle("-fx-background-color: #e3ecf3;"
+								+ "-fx-font: normal 10px 'serif'; -fx-padding: 5px;");
+					}
+				}
+			}
+		}
+		
+		// current cell
+		cellLabel[row][col].setStyle("-fx-background-color: #c0dffb; -fx-font: normal bold 30px 'serif'; "
+					+ "-fx-padding: 5px; -fx-border-color: lightgray;");
+		for (int m = 0; m < 3; m++) {
+			for (int n = 0; n < 3; n++) {
+				notesLabel[row][col][m][n].setStyle("-fx-background-color: #e3ecf3;"
+						+ "-fx-font: normal 10px 'serif'; -fx-padding: 5px;");
+			}
+		}	
 	}
 }

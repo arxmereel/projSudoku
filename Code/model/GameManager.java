bccbef23cf3b4b5763a36a3ad1233ecbc1385f76
EA <82357206+9VD@users.noreply.github.com>
2023-08-07 18:29:55 -0700

    Update GameManager.java

--- Code/model/GameManager.java
+++ Code/model/GameManager.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.Random;
+import java.util.Set;
 
 /**
  * Basic code for the Game Manager Class for Sudoku.
@@ -62,8 +63,8 @@ public class GameManager {
         return false;
     }
 
-    public void addCandidates(int row, int col, int[] cands) {
-        gameBoard.getCell(row, col).addCandidates(cands);
+    public void addCandidate(int row, int col, int cand) {
+        gameBoard.getCell(row, col).addCandidate(cand);
     }
 
     public void clearCandidates(int row, int col) {
@@ -75,15 +76,15 @@ public class GameManager {
     }
 
     // for GUI Parese
-    public void getCellVal(int row, int col) {
-        gameBoard.getCell(row, col).getValue();
+    public int getCellVal(int row, int col) {
+        return gameBoard.getCell(row, col).getValue();
     }
 
     // for GUI Parese
-    public void getCellCands(int row, int col) {
-        gameBoard.getCell(row, col).getCandidates();
+    public Set<Integer> getCellCands(int row, int col) {
+        return gameBoard.getCell(row, col).getCandidates();
     }
-
+    
     public Board getGameBoard() {
 		return gameBoard;
 	}
@@ -118,7 +119,7 @@ public class GameManager {
         removeNumbers(gameBoard);   
         
     }
-
+    
     /**
      * Generates a hard coded Sudoku puzzle.
      */
@@ -129,7 +130,7 @@ public class GameManager {
         solveSudoku(0, 0, getGameBoard());
         answerBoard = new Board(getGameBoard());
         removeNumbers(getGameBoard());   
-        
+
     }
 
     /**
